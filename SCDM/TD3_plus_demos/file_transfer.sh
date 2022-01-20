b30127b0c098bc1197e48d185be1d3cca0f3c3d9
Jianshu-Hu <jianshuhu98@gmail.com>
2022-01-20 09:21:49 +0800

    add a shell file to transfer file from PC to lab cluster

---
+++ SCDM/TD3_plus_demos/file_transfer.sh
@@ -0,0 +1,15 @@
+source=/bigdata/users/jhu/SCDM/SCDM/TD3_plus_demos/saved_fig/
+target=~/PycharmProjects/SCDM/SCDM/TD3_plus_demos/saved_fig/
+
+#source=/bigdata/users/jhu/SCDM/SCDM/TD3_plus_demos/results_critic/
+#target=~/PycharmProjects/SCDM/SCDM/TD3_plus_demos/results_critic/
+
+source2=/bigdata/users/jhu/SCDM/SCDM/TD3_plus_demos/output/
+target2=~/PycharmProjects/SCDM/SCDM/TD3_plus_demos/output/
+
+file_name=*.png
+#file_name1=random_goal_demo_demo_divided_into_two_part_add_auto_regularization.npy
+
+scp jhu@aaal.ji.sjtu.edu.cn:$source$file_name $target
+
+#scp $target jhu@aaal.ji.sjtu.edu.cn:/bigdata/users/jhu/SCDM/SCDM/TD3_plus_demos/new_plot.py
